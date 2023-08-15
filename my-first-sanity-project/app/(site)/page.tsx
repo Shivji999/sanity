b43e87f38fef5b227f4802830254581b602b495c
Shivji999 <shivam_sharma.gptcdiploma@galgotiasunivercity.edu.in>
2023-08-15 18:27:15 +0530

    fix deploying error

--- my-first-sanity-project/app/(site)/page.tsx
+++ my-first-sanity-project/app/(site)/page.tsx
@@ -1,33 +1,27 @@
-import { getProjects } from "@/sanity/sanity-utils"
+import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
-import Link from "next/link"
-
+import Link from "next/link";
 
 export default async function Home() {
-
   const projects = await getProjects();
 
   return (
     <div className="max-w-5xl mx-auto py-20 font-taliwand">
       <h1 className="text-5xl font-extrabold text-purple-700 leading-tight animate-fadeInUp">
-        🚀 Unleash Your Creative Powers<br />with
+        🚀 Unleash Your Creative Powers<br />
         <span className="block text-blue-500">Sanity.io</span>
       </h1>
       <p className="mt-3 text-lg text-gray-600 animate-fadeIn">
-        Embark on an Exciting Journey through <span className="text-yellow-500">Taliwand CSS</span>
+        Embark on an Exciting Journey through{" "}
+        <span className="text-yellow-500">Taliwand CSS</span>
       </p>
-      <p className="mt-3 text-7xl text-gray-600 animate-fadeIn"> My <span className="text-blue-500 animate-bounce">Blogs</span>
+      <p className="mt-3 text-7xl text-gray-600 animate-fadeIn">
+        My <span className="text-blue-500 animate-bounce">Blogs</span>
       </p>
       <div className="mt-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
         {projects.map((project) => (
-    
-          <Link
-          href={`projects/${project.slug}`}
-          >
-            <div
-              key={project._id}
-              className="border border-gray-300 rounded-lg overflow-hidden hover:shadow-xl transform transition duration-300 hover:scale-105 bg-current"
-            >
+          <Link key={project._id} href={`/projects/${project.slug}`}>
+            <div className="border border-gray-300 rounded-lg overflow-hidden hover:shadow-xl transform transition duration-300 hover:scale-105 bg-current">
               {project.image && (
                 <Image
                   src={project.image}
@@ -36,17 +30,16 @@ export default async function Home() {
                   height={150}
                   className="object-cover w-full h-40 rounded-t-lg"
                 />
-              )
-            
-              }
-            
+              )}
               <div className="p-4 bg-gradient-to-r from-purple-600 to-indigo-600 rounded-b-lg">
-                <h2 className="text-white font-bold text-2xl md:text-xl lg:text-2xl xl:text-sm font-sans hover:font-serif">{project.name}</h2>
+                <h2 className="text-white font-bold text-2xl md:text-xl lg:text-2xl xl:text-sm font-sans hover:font-serif">
+                  {project.name}
+                </h2>
               </div>
             </div>
           </Link>
         ))}
       </div>
     </div>
-  )
+  );
 }
